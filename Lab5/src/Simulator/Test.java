d193bc55097f08926aed40f533c7d25f26bbfe72
darudedyrone <davve102@gmail.com>
2018-02-27 14:25:21 +0100

    Test2

--- Lab5/src/Simulator/Test.java
+++ Lab5/src/Simulator/Test.java
@@ -1,11 +1,10 @@
 package Simulator;
 
-import javax.sound.midi.SysexMessage;
-
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("REEEEEEEEEEEEEE");
+		System.out.println("LUL");
 	}
 
 }
